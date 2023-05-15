b340e34e2770030aebaa4dba80435ac56511e824
Sojourner941 <sojourner941@protonmail.com>
2023-05-15 13:51:13 +0800

    c

---
+++ c05/js/event-attributes.js
@@ -0,0 +1,5 @@
+function checkUsername(){
+    let feedbackElement = document.getElementById('feedback')
+    let usernameElement = document.getElementById('username')
+    let username = usernameElement.value
+}
