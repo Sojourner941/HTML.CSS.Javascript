070d595c5d51fafc3124f4fb01eb5233db845e62
Sojourner941 <sojourner941@protonmail.com>
2023-05-15 10:05:27 +0800

    first commit

---
+++ js/inner-text-and-text-content.js
@@ -0,0 +1,11 @@
+var ElementOne = document.getElementById('one')
+//let innerHTML_string = ElementOne.innerHTML
+let innerText_string = ElementOne.innerText
+let textContent_string = ElementOne.textContent
+
+let msg = '<p>textContent:' + textContent_string +'</p>'
+msg += '<p>textContent:' + innerText_string +'</p>'
+
+let elementResults = document.getElementById('scriptResults')
+elementResults.innerHTML = msg
+ElementOne.textContent = '被取代了'
