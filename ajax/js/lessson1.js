3d33d67f8c654e0f26ba4377856aa6696427c004
Sojourner941 <sojourner941@protonmail.com>
2023-05-16 14:53:39 +0800

    c

---
+++ ajax/js/lessson1.js
@@ -0,0 +1,14 @@
+
+function reqListener() {
+    console.log(this.responseText);
+}
+
+const windowload = (event) => {
+    console.log('網頁已經全部被載入');    
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", "http://www.example.org/");
+    req.send();
+}
+
+window.addEventListener('load', windowload)
