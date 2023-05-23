feebd46c208ac5c9cb37e7d8d533b1d498007f3a
Sojourner941 <sojourner941@protonmail.com>
2023-05-23 11:06:47 +0800

    c

---
+++ basic_example/js/basic-example.js
@@ -0,0 +1,10 @@
+let liElements = document.querySelectorAll('li')
+liElements.forEach((element)=>{
+    element.addEventListener('click', (event)=>{
+        let liElement = event.currentTarget
+        $(liElement).hide().fadeIn(1000, ()=>{
+            console.log("hide結束")
+        }).show(1000, ())=>{
+        console.log("show結束")
+    })
+})
